49d8aa0068d12dc5945710accca274bea877f6cc
Khushal Goyal <khushalg670@gmail.com>
2021-04-19 19:46:01 +0000

    Add files via upload

---
+++ Udacity/Machine Learning Engineer with Microsoft Azure Nanodegree Program/Capstone Project/scoring.py
@@ -0,0 +1,29 @@
+import json
+import numpy as np
+import os
+import pickle
+import traceback
+import pandas as pd
+
+
+def init():
+    global model
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "automl_model.pkl")
+    with open(model_path, "rb") as f:
+        model = pickle.load(f)
+
+
+def run(data):
+    try:
+        data = json.loads(data)
+        data = data["data"]
+        data = pd.DataFrame(data)
+        result = model.predict(data)
+        # You can return any data type, as long as it is JSON serializable.
+        # return result.tolist()
+        return json.dumps({"result": result.tolist()})
+
+    except Exception as e:
+        error = str(e)
+        print(traceback.format_exc())
+        return json.dumps({"error": error})
